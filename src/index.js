f9392c4beb86bcad6946e295a15ffd8794a7e619
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-03-07 12:30:26 -0600

    change: logo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const body = document.querySelector('body');
 const sideBar = document.createElement('div');
 
@@ -6,25 +5,41 @@ const products = document.createElement('a');
 const contact = document.createElement('a');
 const signIn = document.createElement('a');
 const share = document.createElement('a');
+const order = document.createElement('a');
 
 products.textContent = 'Productos';
 contact.textContent = 'Contacto';
 signIn.textContent = 'Sign In';
 share.textContent = 'Share';
-
+order.textContent = 'Order';
 
 
 sideBar.appendChild(products);
 sideBar.appendChild(contact);
 sideBar.appendChild(signIn);
 sideBar.appendChild(share);
-
-body.appendChild(sideBar);
+sideBar.appendChild(order);
 
 sideBar.classList.add('side-bar');
 products.classList.add('side-bar-item');
 contact.classList.add('side-bar-item');
 signIn.classList.add('side-bar-item');
 share.classList.add('side-bar-item');
+order.classList.add('side-bar-item');
 
 
+
+// FIX THIS SHIT
+window.addEventListener('scroll', function() {
+    const container = document.querySelector('#sign-up');
+    var position = container.getBoundingClientRect();
+
+    // checking whether fully visible
+    if(position.top >= 0 && position.bottom <= window.innerHeight && sideBar) {
+        console.log('Element is fully visible in screen');
+        body.removeChild(sideBar);
+    }else if(position.top < window.innerHeight && position.bottom >= 0) {  // checking for partial visibility
+        console.log('Element is partially visible in screen');
+        body.appendChild(sideBar);
+    }
+});
